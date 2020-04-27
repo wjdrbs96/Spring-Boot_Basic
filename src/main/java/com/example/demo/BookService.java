dfac018da929b545e3d8f0cf59b747fa2d03b381
최정균 <wjdrbs966@naver.com>
2020-04-27 09:37:01 +0900

    Spring Example + gitignore

---
+++ src/main/java/com/example/demo/BookService.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+public class BookService {
+
+    BookRepository bookRepository;
+
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+}
