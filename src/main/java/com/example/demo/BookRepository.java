ef38b17169eae2b2b55ece04b98cff76bb40691b
최정균 <wjdrbs966@naver.com>
2020-05-01 01:36:10 +0900

    Spring Bean Test

--- src/main/java/com/example/demo/BookRepository.java
+++ src/main/java/com/example/demo/BookRepository.java
@@ -1,4 +1,12 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Repository;
+import java.awt.print.Book;
+
+@Repository
 public class BookRepository {
+
+    public Book save(Book book) {
+        return null;
+    }
 }
--- src/main/java/com/example/demo/BookService.java
+++ src/main/java/com/example/demo/BookService.java
@@ -1,8 +1,17 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BookService {
 
-    BookRepository bookRepository;
+    @Autowired
+    private BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
 
     public void setBookRepository(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
