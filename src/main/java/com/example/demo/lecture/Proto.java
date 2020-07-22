9e56675f6422c4403b642337453ed6452270a258
정균 <wjdrbs966@naver.com>
2020-07-22 16:21:40 +0900

    Spring DI and Bean study

---
+++ src/main/java/com/example/demo/lecture/Proto.java
@@ -0,0 +1,13 @@
+package com.example.demo.lecture;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+@Component @Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class Proto {
+
+    @Autowired
+    Single single;
+}
