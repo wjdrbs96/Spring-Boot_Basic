da00c7525a48b3fee638d2c9f023d402e6810408
정균 <wjdrbs966@naver.com>
2020-07-23 14:43:05 +0900

    Spring framework Resource study

--- src/main/java/com/example/demo/lecture/AppRunner.java
+++ src/main/java/com/example/demo/lecture/AppRunner.java
@@ -3,17 +3,23 @@ package com.example.demo.lecture;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.Validator;
 
 @Component
 public class AppRunner implements ApplicationRunner {
 
     @Autowired
-    ApplicationEventPublisher publisherEvent;
+    ApplicationContext resourceLoader;
 
+    Validator
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        publisherEvent.publishEvent(new MyEvent(this, 100));
+        System.out.println(resourceLoader.getClass());
+        Resource resource = resourceLoader.getResource("text.txt");
+        System.out.println(resource.getClass());
+        System.out.println(resource.exists());
     }
 }
